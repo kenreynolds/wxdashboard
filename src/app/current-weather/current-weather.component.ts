3ca2d8ed7db66ea24d7dbe32d36fcd67b9dcacee
Ken Reynolds <kenreynolds78@gmail.com>
2019-12-08 22:05:57 -0600

    Add simple data existence checking

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import * as moment from 'moment';
 
 import { CurrentWeatherService } from '../core/services/current-weather.service';
+
+import * as moment from 'moment';
 import * as weatherUtils from '../utils/weather-utils';
 
 @Component({
@@ -12,21 +13,20 @@ export class CurrentWeatherComponent implements OnInit {
   isLoading = false;
   wxForecast: any = [];
   wxLocations: any = [];
-  currentWindChill: number;
+  currentWindChill: string;
   currentHumidity: string;
   currentPressure: string;
-  currentTemperature: number;
+  currentTemperature: string;
   currentVisibility: string;
   currentWindSpeed: string;
+  hasWxData: boolean;
   isDaytime: boolean;
   forecastPeriod: string;
   locationName: string;
   observationTime: string;
   state: string;
 
-  constructor(
-    private currentWeatherService: CurrentWeatherService
-  ) { }
+  constructor(private currentWeatherService: CurrentWeatherService) { }
 
   ngOnInit() {
     this.isLoading = true;
@@ -51,12 +51,27 @@ export class CurrentWeatherComponent implements OnInit {
     this.currentWeatherService
       .getWxLocation()
       .subscribe(wxLocationsData => {
-        this.isLoading = false;
         this.wxLocations = wxLocationsData;
-        const baseLocationUrl = this.wxLocations.properties.relativeLocation.properties;
 
-        this.locationName = baseLocationUrl.city;
-        this.state = baseLocationUrl.state;
+        if (this.wxLocations) {
+          this.isLoading = false;
+          console.log('Location data loaded.');
+
+          const baseLocationUrl = this.wxLocations.properties.relativeLocation.properties;
+          this.locationName = baseLocationUrl.city;
+          this.state = baseLocationUrl.state;
+        } else {
+          this.isLoading = false;
+          this.currentWindChill = '--';
+          this.currentHumidity = '--';
+          this.currentPressure = '--';
+          this.currentTemperature = '--';
+          this.currentVisibility = '--';
+          this.currentWindSpeed = '--';
+          this.locationName = '--';
+          this.observationTime = '--';
+          this.state = '--';
+        }
       });
   }
 
@@ -64,23 +79,30 @@ export class CurrentWeatherComponent implements OnInit {
     this.currentWeatherService
       .getWxObservations()
       .subscribe(wxObservationData => {
-        this.isLoading = false;
         if (this.locationName === 'Grand Prairie' && this.state === 'TX') {
           this.wxForecast = wxObservationData;
-          const baseObservationsUrl = this.wxForecast.properties;
 
-          if (baseObservationsUrl.windChill.value === null) {
-            this.currentWindChill = null;
+          if (this.wxForecast && !this.isLoading) {
+            this.hasWxData = true;
+            console.log('Weather data loaded.');
+            const baseObservationsUrl = this.wxForecast.properties;
+
+            if (baseObservationsUrl.windChill.value === null) {
+              this.currentWindChill = null;
+            } else {
+              this.currentWindChill = Math.floor(weatherUtils.convertCelsiusToFahrenheit(baseObservationsUrl.windChill.value)).toString();
+            }
+
+            this.currentHumidity = `${Math.floor(baseObservationsUrl.relativeHumidity.value)}%`;
+            this.currentPressure = `${Math.floor(weatherUtils.convertPascalsToMillibar(baseObservationsUrl.barometricPressure.value))}mb`;
+            this.currentTemperature = Math.floor(weatherUtils.convertCelsiusToFahrenheit(baseObservationsUrl.temperature.value)).toString();
+            this.currentVisibility = `${Math.floor(weatherUtils.convertMetersToMiles(baseObservationsUrl.visibility.value))} miles`;
+            this.currentWindSpeed = `${Math.floor(weatherUtils.convertMetersPerSecondToMilePerHour(baseObservationsUrl.windSpeed.value))} MPH`;
+            this.observationTime = moment.utc(baseObservationsUrl.timestamp).local().format('LT');
           } else {
-            this.currentWindChill = Math.floor(weatherUtils.convertCelsiusToFahrenheit(baseObservationsUrl.windChill.value));
+            this.hasWxData = false;
+            console.log('No weather data to show.');
           }
-
-          this.currentHumidity = `${Math.floor(baseObservationsUrl.relativeHumidity.value)}%`;
-          this.currentPressure = `${Math.floor(weatherUtils.convertPascalsToMillibar(baseObservationsUrl.barometricPressure.value))}mb`;
-          this.currentTemperature = Math.floor(weatherUtils.convertCelsiusToFahrenheit(baseObservationsUrl.temperature.value));
-          this.currentVisibility = `${Math.floor(weatherUtils.convertMetersToMiles(baseObservationsUrl.visibility.value))} miles`;
-          this.currentWindSpeed = `${Math.floor(weatherUtils.convertMetersPerSecondToMilePerHour(baseObservationsUrl.windSpeed.value))} MPH`;
-          this.observationTime = moment.utc(baseObservationsUrl.timestamp).local().format('LT');
         }
       });
   }
