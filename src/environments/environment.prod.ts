5980ab82540bff567d8e30be4f0f3820fa3292ca
Ken Reynolds <kenreynolds78@gmail.com>
2020-11-29 18:18:37 -0600

    Remove env variable from prod; change site title

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,3 @@
 export const environment = {
-  production: true,
-  weatherApiKey: 'd1339026b3444f9390104039201811',
-  currentWeatherApiUrl: 'http://api.weatherapi.com/v1/current.json',
-  forecastWeatherApiUrl: 'http://api.weatherapi.com/v1/forecast.json'
+  production: true
 };
